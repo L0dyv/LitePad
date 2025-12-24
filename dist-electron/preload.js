bda5b26d42d3031185dc73da7043844773fa6e9e
L0dyv <170872410+L0dyv@users.noreply.github.com>
2025-12-24 23:54:01 +0800

    chore: 添加 dist-electron 到 gitignore

--- dist-electron/preload.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-const electron = require("electron");
-electron.contextBridge.exposeInMainWorld("electronAPI", {
-  // 后续添加 IPC 通信方法
-  getVersion: () => process.versions.electron
-});
